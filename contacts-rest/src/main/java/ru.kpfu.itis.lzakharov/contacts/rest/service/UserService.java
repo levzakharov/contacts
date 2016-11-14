41c096f455c4ef9a341dcc56310240ce215e1af3
Lev Zakharov <Lev.Zakharov@icl-services.com>
2016-11-14 15:21:03 +0300

    Add UserService and modify UserRestController

---
+++ contacts-rest/src/main/java/ru.kpfu.itis.lzakharov/contacts/rest/service/UserService.java
@@ -0,0 +1,1 @@
+package ru.kpfu.itis.lzakharov.contacts.rest.service;import ru.kpfu.itis.lzakharov.contacts.rest.model.User;public interface UserService extends CrudService<User> {    boolean isUserExist(User user);}
