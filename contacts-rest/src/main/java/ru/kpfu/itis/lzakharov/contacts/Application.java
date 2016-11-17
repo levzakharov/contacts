6cd251071e32827f76faf7a5031cbe863ff86552
Lev Zakharov <Lev.Zakharov@icl-services.com>
2016-11-17 16:09:22 +0300

    Disable spring-security

--- contacts-rest/src/main/java/ru/kpfu/itis/lzakharov/contacts/Application.java
+++ contacts-rest/src/main/java/ru/kpfu/itis/lzakharov/contacts/Application.java
@@ -1,10 +1,11 @@
 package ru.kpfu.itis.lzakharov.contacts;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 
 import static org.springframework.boot.SpringApplication.run;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class Application {
     public static void main(String[] args) {
         run(Application.class, args);
