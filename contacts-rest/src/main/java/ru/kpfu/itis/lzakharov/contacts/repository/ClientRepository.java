6600207b3c47af93b39efd86fa192584335a0f26
Lev Zakharov <Lev.Zakharov@icl-services.com>
2016-11-17 17:13:30 +0300

    Add ClientRepository

---
+++ contacts-rest/src/main/java/ru/kpfu/itis/lzakharov/contacts/repository/ClientRepository.java
@@ -0,0 +1,11 @@
+package ru.kpfu.itis.lzakharov.contacts.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import ru.kpfu.itis.lzakharov.contacts.model.Client;
+
+@Repository
+public interface ClientRepository extends CrudRepository<Client, Long> {
+    Client findByUsername(String username);
+    Client findByUsernameAndPassword(String username, String password);
+}
